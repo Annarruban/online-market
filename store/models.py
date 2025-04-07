ee8226089a3aeaf30dc3dcf9aa7d9eaf483f4f54
annaruban <anna14196@ukr.net>
2025-04-07 13:51:11 +0200

    task 1

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Categories'
+
